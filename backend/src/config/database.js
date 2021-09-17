2d3292e1cd57989b0ca84465f0105e3a658d8b13
Wellington Santos <weltossousa@gmail.com>
2021-09-16 23:23:46 -0300

    preparing environment to deploy

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,20 @@
 require('dotenv/config');
 
+
 module.exports = {
 	dialect: 'postgres',
+	protocol: 'postgres',
 	host: process.env.APP_HOST,
 	username: process.env.APP_USERNAME,
 	password: process.env.APP_PASSWORD,
 	database: process.env.APP_DATABASE,
 	logging: false,
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false
+		}
+	},
 	define: {
 		timestamps: true,
 		underscored: true,
